a7510d3c416514f3ce64fd57798d89f0ef43bae6
Alex Temnok <temnok@gmail.com>
2026-08-04 22:32:57 -0700

    Test dmstatus register

--- jtag/dmi.go
+++ jtag/dmi.go
@@ -6,18 +6,24 @@ import (
 	"io"
 )
 
+const (
+	DmiOpRead  = 1
+	DmiOpWrite = 2
+)
+
 func SelectDMI(usb io.Writer) {
-	// write 0x11 to IR
+	// JTAG TAP: move to initial state, write 0x11 to IR and move to Idle state
 	nibbles := []byte{
-		0b_0010_0010,
-		0b_0010_0010,
-		0b_0010_0000, // Reset, Idle,
-		0b_0010_0010, // Select-DR, Select-IR,
-		0b_0000_0000, // Capture-IR, Shift-IR,
-		0b_0001_0000, // Shift-IR(1), Shift-IR(0),
-		0b_0000_0000, // Shift-IR(0), Shift-IR(0),
-		0b_0011_0010, // Exit1-IR(1), Update-IR,
-		0b_0000_0000, // Idle, Idle
+		// 4=cap, 2=tms, 1=tdi
+		0x22,
+		0x22,
+		0x20, // 5xTMS==Reset, Idle,
+		0x22, // Select-DR, Select-IR,
+		0x00, // Capture-IR, Shift-IR,
+		0x10, // Shift-IR(1), Shift-IR(0),
+		0x00, // Shift-IR(0), Shift-IR(0),
+		0x32, // Exit1-IR(1), Update-IR,
+		0x00, // Idle, Idle
 	}
 
 	n := check.Err1(usb.Write(nibbles))
@@ -27,38 +33,52 @@ func SelectDMI(usb io.Writer) {
 }
 
 // Debug Module Interface Access (dmi, at 0x11)
-func AccessDMI(usb io.ReadWriter, addr, data, op int) (int, int) {
+func AccessDMI(usb io.ReadWriter, addr, data, op int) (int, int, int) {
+	a, d := addr, data
 	nibbles := []byte{
-		0x20,                                   // Select-DR, Capture-DR,
-		0x00 | byte(op&1),                      // Shift-DR, Shift-DR,
-		0x00 | byte(op>>1&1)<<4 | byte(data&1), // Shift-DR, Shift-DR,
-		0x00 | byte(data>>1&1)<<4 | byte(data>>2&1),   // Shift-DR, Shift-DR,
-		0x00 | byte(data>>3&1)<<4 | byte(data>>4&1),   // Shift-DR, Shift-DR,
-		0x00 | byte(data>>5&1)<<4 | byte(data>>6&1),   // Shift-DR, Shift-DR,
-		0x00 | byte(data>>7&1)<<4 | byte(data>>8&1),   // Shift-DR, Shift-DR,
-		0x00 | byte(data>>9&1)<<4 | byte(data>>10&1),  // Shift-DR, Shift-DR,
-		0x00 | byte(data>>11&1)<<4 | byte(data>>12&1), // Shift-DR, Shift-DR,
-		0x00 | byte(data>>13&1)<<4 | byte(data>>14&1), // Shift-DR, Shift-DR,
-		0x00 | byte(data>>15&1)<<4 | byte(data>>16&1), // Shift-DR, Shift-DR,
-		0x00 | byte(data>>17&1)<<4 | byte(data>>18&1), // Shift-DR, Shift-DR,
-		0x00 | byte(data>>19&1)<<4 | byte(data>>20&1), // Shift-DR, Shift-DR,
-		0x00 | byte(data>>21&1)<<4 | byte(data>>22&1), // Shift-DR, Shift-DR,
-		0x00 | byte(data>>23&1)<<4 | byte(data>>24&1), // Shift-DR, Shift-DR,
-		0x00 | byte(data>>25&1)<<4 | byte(data>>26&1), // Shift-DR, Shift-DR,
-		0x00 | byte(data>>27&1)<<4 | byte(data>>28&1), // Shift-DR, Shift-DR,
-		0x00 | byte(data>>29&1)<<4 | byte(data>>30&1), // Shift-DR, Shift-DR,
-		0x00 | byte(data>>31&1)<<4 | byte(addr&1),     // Shift-DR, Shift-DR,
-		0x00 | byte(addr>>1&1)<<4 | byte(addr>>2&1),   // Shift-DR, Shift-DR,
-		0x00 | byte(addr>>3&1)<<4 | byte(addr>>4&1),   // Shift-DR, Shift-DR,
-		0x02 | byte(addr>>5&1)<<4 | byte(addr>>6&1),   // Shift-DR, Exit1-DR,
-		0x20, // Update-DR, Idle,
-		0x20, // Select-DR, Capture-DR,
-		0x04, // Shift-DR, Shift-DR with capture,
-		0x44, // Shift-DR with capture, Shift-DR with capture,
-
+		0x20,                                    // Select-DR, Capture-DR,
+		0x00 | byte(op&1),                       // Shift-DR, Shift-DR,
+		0x00 | byte(op>>1&1)<<4 | byte(d&1),     // Shift-DR, Shift-DR,
+		0x00 | byte(d>>1&1)<<4 | byte(d>>2&1),   // Shift-DR, Shift-DR,
+		0x00 | byte(d>>3&1)<<4 | byte(d>>4&1),   // Shift-DR, Shift-DR,
+		0x00 | byte(d>>5&1)<<4 | byte(d>>6&1),   // Shift-DR, Shift-DR,
+		0x00 | byte(d>>7&1)<<4 | byte(d>>8&1),   // Shift-DR, Shift-DR,
+		0x00 | byte(d>>9&1)<<4 | byte(d>>10&1),  // Shift-DR, Shift-DR,
+		0x00 | byte(d>>11&1)<<4 | byte(d>>12&1), // Shift-DR, Shift-DR,
+		0x00 | byte(d>>13&1)<<4 | byte(d>>14&1), // Shift-DR, Shift-DR,
+		0x00 | byte(d>>15&1)<<4 | byte(d>>16&1), // Shift-DR, Shift-DR,
+		0x00 | byte(d>>17&1)<<4 | byte(d>>18&1), // Shift-DR, Shift-DR,
+		0x00 | byte(d>>19&1)<<4 | byte(d>>20&1), // Shift-DR, Shift-DR,
+		0x00 | byte(d>>21&1)<<4 | byte(d>>22&1), // Shift-DR, Shift-DR,
+		0x00 | byte(d>>23&1)<<4 | byte(d>>24&1), // Shift-DR, Shift-DR,
+		0x00 | byte(d>>25&1)<<4 | byte(d>>26&1), // Shift-DR, Shift-DR,
+		0x00 | byte(d>>27&1)<<4 | byte(d>>28&1), // Shift-DR, Shift-DR,
+		0x00 | byte(d>>29&1)<<4 | byte(d>>30&1), // Shift-DR, Shift-DR,
+		0x00 | byte(d>>31&1)<<4 | byte(a&1),     // Shift-DR, Shift-DR,
+		0x00 | byte(a>>1&1)<<4 | byte(a>>2&1),   // Shift-DR, Shift-DR,
+		0x00 | byte(a>>3&1)<<4 | byte(a>>4&1),   // Shift-DR, Shift-DR,
+		0x02 | byte(a>>5&1)<<4 | byte(a>>6&1),   // Shift-DR, Exit1-DR,
+		0x20,                                    // Update-DR, Idle,
+		0x00,                                    // Idle, Idle,
+		0x02,                                    // Idle, Select-DR,
+		0x00,                                    // Capture-DR, Shift-DR,
+		0x4F,                                    // Shift-DR with capture, 7 x Shift-DR with capture
+		0xDE,                                    // 32 x Shift-DR with capture,
+		0x62,                                    // Exit1-DR with capture, Update-DR,
+		0x0A,                                    // Idle, Flush
 	}
 
-	_ = nibbles
+	n := check.Err1(usb.Write(nibbles))
+	if n != len(nibbles) {
+		panic(fmt.Errorf("AccessDMI/usb.Write() returned %v, expected %v", n, len(nibbles)))
+	}
 
-	return 0, 0
+	r := make([]byte, 6)
+	n = check.Err1(usb.Read(r))
+	if n != len(r) {
+		panic(fmt.Errorf("AccessDMI/usb.Read() returned %v, expected %v", n, len(nibbles)))
+	}
+
+	resp := int(r[5])<<40 | int(r[4])<<32 | int(r[3])<<24 | int(r[2])<<16 | int(r[1])<<8 | int(r[0])
+	return resp >> 34, int(uint32(resp >> 2)), resp & 3
 }
--- tests/dmi_dmstatus_test.go
+++ tests/dmi_dmstatus_test.go
@@ -1,17 +1,21 @@
 package tests
 
 import (
+	"github.com/stretchr/testify/assert"
 	"github.com/temnok/esp32c6/jtag"
 	"io"
 	"testing"
 )
 
-func xTestDmiDmstatus(t *testing.T) {
+func TestDmiDmstatus(t *testing.T) {
 	defer handlePanic(t)
 
 	jtag.WithUsbConnection(func(usb io.ReadWriter) {
-		//data, op := jtag.AccessDMI(usb, 0x11<<34|0<<2|1)
-		//assert.Equal(t, 0, op)
-		//fmt.Printf("dmstatus = %032b\naddr = %07b\n", dmstatus, addr)
+		jtag.SelectDMI(usb)
+
+		addr, dmstatus, op := jtag.AccessDMI(usb, 0x11, 0, jtag.DmiOpRead)
+		assert.Equal(t, 0, op)
+		assert.Equal(t, 2, dmstatus&0xF) // 2 (0.13): There is a Debug Module and it conforms to version 0.13 of this specification
+		assert.Equal(t, 0x11, addr)
 	})
 }
