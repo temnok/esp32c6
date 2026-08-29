c699f95cfff852ced9c84c48f318d8012071277d
Alex Temnok <temnok@gmail.com>
2026-08-29 11:17:24 -0700

    Refactor

--- gcc/probe/probe.c
+++ gcc/probe/probe.c
@@ -6,38 +6,9 @@
 __attribute__((naked, section(".text._vectors")))
 void _vectors() {
     __asm__ volatile (
-        "j trap_handler\n" //  0
-        "j trap_handler\n" //  1
-        "j trap_handler\n" //  2
-        "j trap_handler\n" //  3
-        "j trap_handler\n" //  4
-        "j trap_handler\n" //  5
-        "j trap_handler\n" //  6
-        "j trap_handler\n" //  7
-        "j trap_handler\n" //  8
-        "j trap_handler\n" //  9
-        "j trap_handler\n" // 10
-        "j trap_handler\n" // 11
-        "j trap_handler\n" // 12
-        "j trap_handler\n" // 13
-        "j trap_handler\n" // 14
-        "j trap_handler\n" // 15
-        "j trap_handler\n" // 16
-        "j trap_handler\n" // 17
-        "j trap_handler\n" // 18
-        "j trap_handler\n" // 19
-        "j trap_handler\n" // 20
-        "j trap_handler\n" // 21
-        "j trap_handler\n" // 22
-        "j trap_handler\n" // 23
-        "j trap_handler\n" // 24
-        "j trap_handler\n" // 25
-        "j trap_handler\n" // 26
-        "j trap_handler\n" // 27
-        "j trap_handler\n" // 28
-        "j trap_handler\n" // 29
-        "j trap_handler\n" // 30
-        "j trap_handler\n" // 31
+        ".rept 32\n"
+        "   j trap_handler\n"
+        ".endr\n"
     );
 }
 
