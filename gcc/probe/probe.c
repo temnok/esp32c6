a9838870f479b7c9ad825f05c9ca444211981010
Alex Temnok <temnok@gmail.com>
2026-09-09 03:25:37 -0700

    Light LED

--- gcc/probe/probe.c
+++ gcc/probe/probe.c
@@ -12,18 +12,23 @@ void _vectors() {
     );
 }
 
-int static_var;
+const unsigned GPIO_MATRIX_BASE_ADDR = 0x60091000;
+
+unsigned
+    * const GPIO_OUT_W1TS_REG = (unsigned*)(GPIO_MATRIX_BASE_ADDR + 0x0008),
+    * const GPIO_ENABLE_W1TS_REG = (unsigned*)(GPIO_MATRIX_BASE_ADDR + 0x0024),
+    * const GPIO_FUNC_OUT_SEL_CFG_REG = (unsigned*)(GPIO_MATRIX_BASE_ADDR + 0x0554);
 
 __attribute__((section(".text._start")))
 void _start() {
     extern int _bss_start, _bss_end;
-    for (int *p = &_bss_start; p < &_bss_end; p++) {
-        *p = 0;
-    }
+    for (int *p = &_bss_start; p < &_bss_end; p++) *p = 0;
 
-    fmt_str(sys_print, "Hello, world: ");
-    fmt_int(sys_print, static_var);
-    fmt_str(sys_print, "\n");
+//    fmt_str(sys_print, "Reg value: ");
+//    fmt_unsigned_hex(sys_print, GPIO_FUNC_OUT_SEL_CFG_REG[15]);
+//    fmt_str(sys_print, "\n");
+    *GPIO_ENABLE_W1TS_REG = 1<<15;
+    *GPIO_OUT_W1TS_REG = 1<<15;
 
     sys_exit();
 }
