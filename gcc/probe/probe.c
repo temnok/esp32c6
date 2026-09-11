bae1cb5f35292b2058ba625f5c273e194c029e1f
Alex Temnok <temnok@gmail.com>
2026-09-10 20:44:10 -0700

    Work on failures after erasing flash

--- gcc/probe/probe.c
+++ gcc/probe/probe.c
@@ -54,12 +54,17 @@ void _start() {
     extern int _bss_start, _bss_end;
     for (int *p = &_bss_start; p < &_bss_end; p++) *p = 0;
 
-    ((volatile int*)GPIO_FUNC_OUT_SEL_CFG_REG)[8] = 0x80;
-    *(volatile int*)GPIO_ENABLE_W1TS_REG = 1<<8;
+//    ((volatile int*)GPIO_FUNC_OUT_SEL_CFG_REG)[8] = 0x80;
+//    *(volatile int*)GPIO_ENABLE_W1TS_REG = 1<<8;
+//    for (int i = 0; i < 1; i++) {
+//        rainbow(8, 1'600'000);
+//    }
 
-    for (int i = 0; i < 1; i++) {
-        rainbow(8, 1'600'000);
-    }
+    ((volatile int*)GPIO_FUNC_OUT_SEL_CFG_REG)[15] = 0x80;
+    *(volatile int*)GPIO_ENABLE_W1TS_REG = 1<<15;
+
+    *(volatile int*)GPIO_OUT_REG = 1<<15;
+    sleep_cycles(16'000'000);
 
     sys_exit();
 }
